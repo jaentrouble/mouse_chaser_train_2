b28d2e2f37581a2e863252cdff08cc755deee810
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-11-24 11:25:47 +0900

    direct call + numpy

--- model_trainer.py
+++ model_trainer.py
@@ -285,13 +285,13 @@ class ValFigCallback(keras.callbacks.Callback):
             gt_box = gt_box[0]
             h,w = np.subtract(gt_image.shape[:2],1)
             for roi in rois:
-                x1, y1, x2, y2 = (roi*[w,h,w,h,]).astype(np.int64)
+                x1, y1, x2, y2 = np.multiply(roi,[w,h,w,h,]).astype(np.int64)
                 test_image[y1,x1:x2] = [0,1,0]
                 test_image[y2,x1:x2] = [0,1,0]
                 test_image[y1:y2,x1] = [0,1,0]
                 test_image[y1:y2,x2] = [0,1,0]
             for box in gt_box:
-                x1, y1, x2, y2 = (box*[w,h,w,h,]).astype(np.int64)
+                x1, y1, x2, y2 = np.multiply(box,[w,h,w,h,]).astype(np.int64)
                 gt_image[y1,x1:x2] = [1,0,0]
                 gt_image[y2,x1:x2] = [1,0,0]
                 gt_image[y1:y2,x1] = [1,0,0]
