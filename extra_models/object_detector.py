1fa5dd353f043ca6e1300d0f86415d8c98fe975b
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-11-24 14:14:47 +0900

    reg to zero

--- extra_models/object_detector.py
+++ extra_models/object_detector.py
@@ -123,6 +123,10 @@ class ObjectDetector(keras.Model):
         feature_map = self.inter_conv(features)
         cls_score = self.cls_conv(feature_map)
         bbox_reg = self.reg_conv(feature_map)
+        #-----------DEBUG
+        bbox_reg = tf.zeros_like(bbox_reg)
+        #---------------------------------
+        
         boxes, soft_probs = self.proposal(cls_score, bbox_reg)
 
         #------------DEBUG
