e404479bf6af94c5d75360d9ec688254722179c1
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-11-24 13:26:48 +0900

    reshape

--- extra_models/object_detector.py
+++ extra_models/object_detector.py
@@ -125,6 +125,7 @@ class ObjectDetector(keras.Model):
         # boxes, soft_probs = self.proposal(cls_score, bbox_reg)
         #------------DEBUG
         _, boxes, _ = self.anchor_target(gt)
+        boxes = tf.reshape(boxes,(-1,4))
         soft_probs = tf.zeros(10)
 
 
